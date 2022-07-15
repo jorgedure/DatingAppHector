14d61bba74ce5184037fa0f885dc822dc9fdbe2e
jorgedure <53130457+jorgedure@users.noreply.github.com>
2022-07-15 07:36:50 -0400

    End of section 7

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,5 +1,5 @@
 using System.Text;
-    using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -19,56 +19,52 @@ using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using API.Extensions;
+using API.Middleware;
 
 namespace API
 {
-    public class Startup
-    {
+  public class Startup
+  {
     private readonly IConfiguration _config;
 
-        public Startup(IConfiguration config)
-        {
+    public Startup(IConfiguration config)
+    {
       _config = config;
 
-        }
+    }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
     {
-             
-            services.AddApplicationServices(_config);
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-                 {  
-                  c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
-                 });
-            services.AddIdentityServices(_config);
+
+      services.AddApplicationServices(_config);
+      services.AddControllers();
+      services.AddSwaggerGen(c =>
+           {
+             c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
+           });
+      services.AddIdentityServices(_config);
     }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
-            }
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      app.UseMiddleware<ExceptionMiddleware>();
+      
+      app.UseHttpsRedirection();
 
-            app.UseHttpsRedirection();
+      app.UseRouting();
 
-            app.UseRouting();
+      app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
-            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
-            
-            app.UseAuthentication();
-            
-            app.UseAuthorization();
+      app.UseAuthentication();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
     }
+  }
 }
