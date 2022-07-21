a205075760c8c0148438eafcf9b95fe1f97080b1
jorgedure <53130457+jorgedure@users.noreply.github.com>
2022-07-21 00:15:14 -0400

    End Section  12

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 using API.DTOs;
 using Microsoft.EntityFrameworkCore;
 using API.Interfaces;
+using AutoMapper;
 
 namespace API.Controllers
 {
@@ -17,8 +18,10 @@ namespace API.Controllers
   {
     private readonly DataContext _context;
     private readonly ITokenService _tokenService;
-    public AccountController(DataContext context, ITokenService tokenService)
+    private readonly IMapper _mapper;
+    public AccountController(DataContext context, ITokenService tokenService, IMapper mpapper)
     {
+      _mapper = mpapper;
       _tokenService = tokenService;
       _context = context;
     }
@@ -28,23 +31,24 @@ namespace API.Controllers
     {
       if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
 
+      var user = _mapper.Map<AppUser>(registerDto);
+
       using var hmac = new HMACSHA512();
 
-      var user = new AppUser
-      {
-        UserName = registerDto.Username.ToLower(),
-        PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
-        PasswordSalt = hmac.Key
-      };
+      user.UserName = registerDto.Username.ToLower();
+      user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
+      user.PasswordSalt = hmac.Key;
+
 
       _context.Users.Add(user);
       await _context.SaveChangesAsync();
 
       return new UserDto
       {
-         Username = user.UserName,
-         Token = _tokenService.CreateToken(user)
-      };
+        Username = user.UserName,
+        Token = _tokenService.CreateToken(user),
+        KnowAs = user.KnownAs
+      };  
     }
 
     [HttpPost("login")]
@@ -69,7 +73,8 @@ namespace API.Controllers
       {
         Username = user.UserName,
         Token = _tokenService.CreateToken(user),
-        PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
+        PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
+        KnowAs = user.KnownAs
       };
     }
 
@@ -77,5 +82,5 @@ namespace API.Controllers
     {
       return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
     }
-}
+  }
 }
--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,20 +1,20 @@
-using System.Reflection.Metadata;
-using System.Runtime.CompilerServices;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 
 namespace API.DTOs
 {
-    public class RegisterDto
-    {
-        [Required]
-        public string Username { get; set; }
+  public class RegisterDto
+  {
+    [Required] public string Username { get; set; }
+    [Required] public string KnowAs { get; set; }
+    [Required] public string Gender { get; set; }
+    [Required] public DateTime DateOfBirth { get; set; }
+    [Required] public string City { get; set; }
+    [Required] public string Country { get; set; }
 
-        [Required]
-        [StringLength(8, MinimumLength = 4)]
-        public string Password { get; set; }
-    }
+
+    [Required]
+    [StringLength(8, MinimumLength = 4)]
+    public string Password { get; set; }
+  }
 }
